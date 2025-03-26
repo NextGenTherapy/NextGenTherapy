5b3cbe2898d0e28fa83d035f56044694018849dd
Luke Stevens <lukestevens@Lukes-Laptop.local>
2025-03-26 15:33:59 +0000

    Update header components and css styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,12 +5,17 @@ export default function Header() {
     return (
         <header className={styles.header}>
             <nav className={styles.nav}>
+                {/* Logo Section */}
+                <div className={styles.logo}>
+                    <Link href="/">
+                        <h1>NextGenTherapy</h1>
+                    </Link>
+                </div>
+
+                {/* Nav bar links */}
                 <ul className={styles.navList}>
                     <li className={styles.navItem}>
                         <Link href="/">Home</Link>
-                    </li>
-                    <li className={styles.navItem}>
-                        <Link href="/about">About</Link>
                     </li>
                     <li className={styles.navItem}>
                         <Link href="/therapy-101">Therapy 101</Link>
@@ -20,6 +25,9 @@ export default function Header() {
                     </li>
                     <li className={styles.navItem}>
                         <Link href="/clientele">Who I see</Link>
+                    </li>
+                    <li className={styles.navItem}>
+                        <Link href="/about">About</Link>
                     </li>
                     <li className={styles.navItem}>
                         <Link href="/services">Services</Link>
