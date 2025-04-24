4ddda0cfdff7aedd4c53a5046b33cff1519c8f7a
Luke Stevens <lukestevens@Lukes-Laptop.local>
2025-04-24 17:27:54 +0100

    added media query for footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,85 +7,107 @@ export default function Footer() {
         <footer className={styles.footer}>
             <div className={styles.footerContent}>
                 <div className={styles.footerColumn}>
-                <Link href="/contact">
+                    <Link href="/contact">
                         <h3>Location</h3>
                     </Link>
-                <ul>
-                    <li>
-                        <a href="https://www.google.com/maps?q=Colchester+Business+Centre,+1+George+Williams+Way,+Colchester+CO1+2JS"
+                    <ul>
+                        <li>
+                            <a
+                                href="https://www.google.com/maps?q=Colchester+Business+Centre,+1+George+Williams+Way,+Colchester+CO1+2JS"
                                 target="_blank"
-                                rel="noopener noreferrer">
-                                Colchester Business Centre, 1 George Williams Way, Colchester CO1 2JS
+                                rel="noopener noreferrer"
+                            >
+                                <span>Colchester Business Centre,</span>
+                                <span>1 George Williams Way,</span>
+                                <span>Colchester</span>
+                                <span>CO1 2JS</span>
                             </a>
                         </li>
-                    <li>
-                    <a
+                        <li>
+                            <a
                                 href="https://www.google.com/maps?q=The+Henry+Centre,+Rochester+House,+145+New+London+Rd,+Chelmsford+CM2+0QT"
                                 target="_blank"
-                                rel="noopener noreferrer">
-                        The Henry Centre, Rochester House, 145 New London Rd, Chelmsford CM2 0QT
-                        </a>
+                                rel="noopener noreferrer"
+                            >
+                                <span>The Henry Centre, Rochester House,</span>
+                                <span>145 New London Rd,</span>
+                                <span>Chelmsford</span>
+                                <span>CM2 0QT</span>
+                            </a>
                         </li>
-                </ul>
-            </div>
+                    </ul>
+                </div>
 
-            <div className={styles.footerColumn}>
-            <Link href="/contact">
+                <div className={styles.footerColumn}>
+                    <Link href="/contact">
                         <h3>Hours</h3>
                     </Link>
-                <ul className="weekday">
-                    <li>
-                        Monday - Tuesday
-                    </li>
-                    <li>
-                    10am - 7pm 
-                    </li>
-                    <li>
-                    Friday
-                    </li>
-                    <li>
-                    9am-2pm 
-                    </li>
-                    <li>
-                    Saturday - Chelmsford
-                    </li>
-                    <li>
-                    9am - 3pm 
-                    </li>
-                </ul>
-            </div>
+                    <ul className="weekday">
+                        <li>Monday - Tuesday</li>
+                        <li>10am - 7pm</li>
+                        <li>Friday</li>
+                        <li>9am-2pm</li>
+                        <li>Saturday - Chelmsford</li>
+                        <li>9am - 3pm</li>
+                    </ul>
+                </div>
 
-            <div className={styles.footerColumn}>
-            <Link href="/contact">
+                <div className={styles.footerColumn}>
+                    <Link href="/contact">
                         <h3>Contact me</h3>
                     </Link>
-                <p>
-                    Email: <a href="mailto:andreeatherapytoday@gmail.com">andreeatherapytoday@gmail.com</a>
-                </p>
-                <p>
-                    Phone: <a href="tell:+447448036017">+447448036017</a>
-                </p>
-                <div className={styles.socialLinks}>
-                    <a href="https://www.facebook.com/share/16dg5gpZRo/?mibextid=wwXIfr" 
-                        target="_blank" 
-                        rel="noopener noreferrer"
-                        title="Vist our Facebook page">
-                        <Image src="/images/facebook.png" alt="Instagram" width={40} height={40} />
-                    </a>
-                    <a href="https://www.instagram.com/nextgentherapycolchester?igsh=MWx2N2g0NnI0eTVveQ%3D%3D&utm_source=qr" 
-                        target="_blank" 
-                        rel="noopener noreferrer"
-                        title="Vist our Instagram page">
-                        <Image src="/images/instagram.png" alt="Instagram" width={40} height={40} />
-                    </a>
-                    <a href="https://www.bacp.co.uk/therapists/385976/andreea-horhocea/london-e16" 
-                       target="_blank" 
-                       rel="noopener noreferrer"
-                       title="Vist our BACP page">
-                        <Image src="/images/bacp.png" alt="Instagram" width={40} height={40} />
-                    </a>
+                    <p>
+                        Email:{" "}
+                        <a href="mailto:andreeatherapytoday@gmail.com">
+                            andreeatherapytoday@gmail.com
+                        </a>
+                    </p>
+                    <p>
+                        Phone:{" "}
+                        <a href="tel:+447448036017">+447448036017</a>
+                    </p>
+                    <div className={styles.socialLinks}>
+                        <a
+                            href="https://www.facebook.com/share/16dg5gpZRo/?mibextid=wwXIfr"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            title="Visit our Facebook page"
+                        >
+                            <Image
+                                src="/images/facebook.png"
+                                alt="Facebook"
+                                width={40}
+                                height={40}
+                            />
+                        </a>
+                        <a
+                            href="https://www.instagram.com/nextgentherapycolchester?igsh=MWx2N2g0NnI0eTVveQ%3D%3D&utm_source=qr"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            title="Visit our Instagram page"
+                        >
+                            <Image
+                                src="/images/instagram.png"
+                                alt="Instagram"
+                                width={40}
+                                height={40}
+                            />
+                        </a>
+                        <a
+                            href="https://www.bacp.co.uk/therapists/385976/andreea-horhocea/london-e16"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            title="Visit our BACP page"
+                        >
+                            <Image
+                                src="/images/bacp.png"
+                                alt="BACP"
+                                width={40}
+                                height={40}
+                            />
+                        </a>
+                    </div>
                 </div>
-            </div>
             </div>
         </footer>
     );
