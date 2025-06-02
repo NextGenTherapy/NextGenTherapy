901b5a6ab4a91ed185dbf67a4dd6978296ef3eb4
Luke Stevens <lukestevens@Lukes-Laptop.local>
2025-06-02 19:58:00 +0100

    added vercel speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 import { Analytics } from "@vercel/analytics/next";
 import { GoogleAnalytics } from '@next/third-parties/google';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 
 export const metadata: Metadata = {
@@ -91,6 +92,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
       {/* Google Analytics script with nonce */}
       {/* If you want to use nonce for GoogleAnalytics, you can add it back here */}
       <GoogleAnalytics gaId="G-3528EDPEXW" />
+      <SpeedInsights />
     </html>
   );
 }
